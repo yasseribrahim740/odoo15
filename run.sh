a6693141442d5996db8b0d2994f9c543bed982a4
Yasser Ibrahim <112755454+yasseribrahim740@users.noreply.github.com>
2024-02-07 09:59:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
 sudo chmod -R 777 $DESTINATION
-cp ./addons $DESTINATION/addons
+cp -r ./addons/* $DESTINATION/addons/
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
