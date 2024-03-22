bf604cf7ffc6ce33c496fefc31a0363c1a8fbee4
Yasser Ibrahim <112755454+yasseribrahim740@users.noreply.github.com>
2024-03-23 01:35:00 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,6 +15,6 @@ sudo sysctl -p
 sed -i 's/10015/'$PORT'/g' $DESTINATION/docker-compose.yml
 sed -i 's/20015/'$CHAT'/g' $DESTINATION/docker-compose.yml
 # run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml up -d
+docker compose -f $DESTINATION/docker-compose.yml up -d
 
 echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: l1N746hawOrI | Live chat port: '$CHAT
